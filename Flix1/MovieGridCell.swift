48086dcdc190692b9bf47f08683b635d56b0deaf
bryan f <bfowles@csumb.edu>
2021-02-15 14:17:03 -0800

    collection view setup

---
+++ Flix1/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix1
+//
+//  Created by user190937 on 2/15/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
