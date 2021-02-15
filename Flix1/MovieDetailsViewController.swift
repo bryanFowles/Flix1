5ffc6e6f8f07889233d1be5a177f1c62ddf47cb3
bryan f <bfowles@csumb.edu>
2021-02-14 18:09:17 -0800

    fixed images added navigation details

---
+++ Flix1/MovieDetailsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MovieDetailsViewController.swift
+//  Flix1
+//
+//  Created by user190937 on 2/14/21.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+    
+    var movie: [String:Any]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
